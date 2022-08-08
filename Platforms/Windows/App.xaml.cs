a909bf0237049de2eac713a0138e1ccfdfe066ed
Maksim <6383127@gmail.com>
2022-08-09 00:13:03 +0300

    Fix startup

--- Platforms/Windows/App.xaml.cs
+++ Platforms/Windows/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.UI.Xaml;
+﻿using EmailAuth;
+using Microsoft.UI.Xaml;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -10,22 +11,15 @@ namespace EmailAuth.WinUI;
 /// </summary>
 public partial class App : MauiWinUIApplication
 {
-    /// <summary>
-    /// Initializes the singleton application object.  This is the first line of authored code
-    /// executed, and as such is the logical equivalent of main() or WinMain().
-    /// </summary>
-    public App()
-    {
-        this.InitializeComponent();
-    }
+	/// <summary>
+	/// Initializes the singleton application object.  This is the first line of authored code
+	/// executed, and as such is the logical equivalent of main() or WinMain().
+	/// </summary>
+	public App()
+	{
+		this.InitializeComponent();
+	}
 
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-
-    protected override void OnLaunched(LaunchActivatedEventArgs args)
-    {
-        base.OnLaunched(args);
-
-        Platform.OnLaunched(args);
-    }
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
 
